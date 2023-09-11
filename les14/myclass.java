bfd0a4ffb6f0b558350f94108b14d99e20f1b7d0
hades <69485304+joeeveren@users.noreply.github.com>
2023-09-11 18:56:17 +0600

    Update myclass.java

--- les14/myclass.java
+++ les14/myclass.java
@@ -2,68 +2,68 @@ import java.io.*;
 import java.util.Random;
 
 public class myclass {
-    private static final String LOG_FILE_PATH = "error.log"; // Путь к лог-файлу
     public static final Random rand = new Random();
 
     public static void main(String[] args) {
-
-        // задание 3.1
-        for (int i = 1; i < 11; i++) {
-            String filePath = i + ".txt";
-            File myFile = new File(filePath);
-
-            try {
-                BufferedWriter bw = new BufferedWriter(new FileWriter(myFile));
-                bw.write(String.valueOf(rand.nextInt(100)));
-                bw.newLine();
-                bw.write(String.valueOf(rand.nextInt(100)));
-                bw.newLine();
-                bw.write(String.valueOf(rand.nextInt(100)));
-                bw.close();
-            } catch (IOException e) {
-                logError("ошибка при создании файла: " + e.getMessage());
-            }
-        }
-
-        // задание 3.2
         try {
-            int sum = sumFromFile("");
-            System.out.println("Сумма всех чисел: " + sum);
+            int[] result = sumFromRandomFiles("");
+            if (result[0] == 0) {
+                System.out.println("Сумма всех чисел: " + result[1]);
+                System.out.println(result[0]);
+            } else {
+                System.out.println("Ошибка при суммировании чисел. Код ошибки: " + result[0]);
+            }
         } catch (IOException e) {
-            logError("ошибка при суммировании чисел: " + e.getMessage());
+            System.out.println("Ошибка при суммировании чисел: " + e.getMessage());
         }
     }
 
-    public static int sumFromFile(String pathPrefix) throws IOException {
+    public static int[] sumFromRandomFiles(String pathPrefix) throws IOException {
+        int[] result = new int[2];
         int sum = 0;
+
         int n1 = rand.nextInt(10) + 1;
-        int n2 = rand.nextInt(10) + 1;
+        int n2;
+        do {
+            n2 = rand.nextInt(10) + 1;
+        } while (n1 == n2);
 
         for (int i : new int[]{n1, n2}) {
             String filePath = pathPrefix + i + ".txt";
             File myFile = new File(filePath);
+
+            if (!myFile.exists()) {
+                result[0] = 1; // Код ошибки 1: Файл не существует
+                result[1] = 0; // Сумма равна 0, так как файла нет
+                return result;
+            }
+
             BufferedReader br = new BufferedReader(new FileReader(myFile));
-            int g = 3;
-            while (g > 0) {
-                String line = br.readLine();
-                if (line == null) {
-                    logError("файл " + myFile.getName() + " неполное.");
-                    throw new IOException("Файл " + myFile.getName() + " неполное.");
+            int linesRead = 0;
+            String line;
+            while ((line = br.readLine()) != null) {
+                try {
+                    int number = Integer.parseInt(line);
+                    sum += number;
+                    linesRead++;
+                } catch (NumberFormatException e) {
+                    result[0] = 1; // Код ошибки 3: Файл содержит неправильные данные
+                    result[1] = 0; // Сумма равна 0, так как файл содержит ошибку
+                    br.close();
+                    return result;
                 }
-                sum += Integer.parseInt(line);
-                g--;
             }
             br.close();
-        }
-        return sum;
-    }
 
-    private static void logError(String message) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(LOG_FILE_PATH, true))) {
-            bw.write(message);
-            bw.newLine();
-        } catch (IOException e) {
-            System.err.println("ошибка при записи в лог файл: " + e.getMessage());
+            if (linesRead != 3) {
+                result[0] = 2; // Код ошибки 2: Файл неполный
+                result[1] = 0; // Сумма равна 0, так как файл неполный
+                return result;
+            }
         }
+
+        result[0] = 0; // Успешное выполнение
+        result[1] = sum; // Сумма чисел
+        return result;
     }
 }
