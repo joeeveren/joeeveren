ce4679c2d409d99846a53db854d54792eb5e3e4c
hades <69485304+joeeveren@users.noreply.github.com>
2023-09-13 15:48:44 +0600

    Update lesson2.java

--- z28/lesson2.java
+++ z28/lesson2.java
@@ -1,11 +1,12 @@
 import java.util.*;
-public class lesson2 {
-public static int odometer(int[] oksana) {
-    int w = 0;
-    for (int i = 0; i < oksana.length; i += 2) {
-        if     (i == 0)  w = w + oksana[i] * oksana [i + 1]; 
-        else             w = w + oksana[i] * (oksana [i + 1] - oksana [i - 1]);
-    } 
-    return w ;
-}
+public class lesson2 
+{
+    public static int odometer(int [] oksana)  {
+        int w = 0;
+        for (int i = 0; i < oksana.length; i += 2) {
+            if     (i == 0)  w = w + oksana[i] * oksana [i + 1];
+            else             w = w + oksana[i] * (oksana [i + 1] - oksana [i - 1]);
+        }
+        return w ;
+    }
 }
