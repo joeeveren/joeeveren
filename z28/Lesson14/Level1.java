9eaf94275c3471bc316cafffa10926a80b01b6af
hades <69485304+joeeveren@users.noreply.github.com>
2023-10-04 09:41:50 +0600

    Update Level1.java
    wroks for case Unmanned(10, 2, [[11,5,5],[15,2,2]])

--- z28/Lesson14/Level1.java
+++ z28/Lesson14/Level1.java
@@ -3,7 +3,12 @@ public class Level1 {
     public static int Unmanned(int L, int N, int [][] track) {
         int s = 0;
         int k = 0;
+        int f = 0;
         for (int i = 0; i < track.length; i ++){
+            if (track[i][0] >= L) {
+              return s + L - f;  
+            }
+            f = track[i][0];
             k = track[i][0]  - k;
             int t = (s + k ) % (track[i][1] + track[i][2]); 
             int l = (t < track[i][1]) ? track[i][1] : k;
