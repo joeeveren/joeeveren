0bda352b5f355452e604221d1e5c26dc269c16a2
hades <69485304+joeeveren@users.noreply.github.com>
2023-10-10 21:17:06 +0600

    Create Level1.java
    lesson 16

---
+++ z28/lesson16/Level1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Level1 {
+    public static int [] sorted( int [] m) {
+        int b = m.length;
+        while ( b > 0){
+            for(int i = 0; i < b - 1; i ++){
+                if (m[i] < m[i + 1]) {
+                    int e = m[i];
+                    m[i] = m[i + 1];
+                    m[i + 1] = e;
+                }
+            }  b = b - 1;
+        }
+        return m;
+    }
+    public static int MaximumDiscount(int N, int [] price) {
+        int [] p = sorted(price);
+        int sum = 0;
+        for (int i = 0; i < N; i ++) {
+            if (i % 3 != 2) {
+                sum = sum + p[i];
+            }
+        }
+        return sum;
+    }
+
+}
