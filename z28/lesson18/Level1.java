6eb9c4df1b7e751a11aa67f4ef623642708a36ff
hades <69485304+joeeveren@users.noreply.github.com>
2023-10-17 12:35:40 +0600

    Update Level1.java
    предыдущую версию сказали нельзя использовать

--- z28/lesson18/Level1.java
+++ z28/lesson18/Level1.java
@@ -1,18 +1,31 @@
-import java.util.*;
-
 public class Level1 {
-    public static boolean MisterRobot(int N, int [] data) {
-        int s = 0;
-        for (int i = 0; i < N - 1; i ++) {
-            for (int j = i + 1; j < N; j ++ ){
-                if ( data[i]> data [j]) {
-                    s = s + 1;
-                }
+    public static boolean MisterRobot(int N, int[] data) {
+        
+        int d = 1;
+        while (d <= N){
+            int i = 0;
+        while (i < N - 2) {
+            int a = data[i];
+            int b = data[i + 1];
+            int c = data[i + 2];
+
+            if (a < b && b < c) {
+                i++;
+            } else  {
+                int temp = a;
+                data[i] = b;
+                data[i + 1] = c;
+                data[i + 2] = temp;
+        }
+        }
+      
+               d += 1;
+        }
+        for (int j = 1; j < N; j++) {
+            if (data[j] < data[j - 1]) {
+                return false; 
             }
         }
-        if ( s % 2 == 0) {
-            return true;
-        }
-        return false;
+        return true; 
     }
 }
